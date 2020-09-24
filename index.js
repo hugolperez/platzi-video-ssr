2edd6e2d61463bd0814b7ebcde3deaf8addd49c4
hugolperez <hperez.hugoleonel@gmail.com>
2020-09-23 20:24:36 -0600

    OAuth2.0 with Google implemented

--- index.js
+++ index.js
@@ -15,15 +15,18 @@ app.use(cookieParser());
 // Basic strategy
 require('./utils/auth/strategies/basic');
 
-app.post('/auth/sign-in', async function(req, res, next) {
-  passport.authenticate('basic', function(error, data) {
+//Oauth strategy
+require('./utils/auth/strategies/oauth');
+
+app.post('/auth/sign-in', async function (req, res, next) {
+  passport.authenticate('basic', function (error, data) {
     try {
-      if(error || !data) {
+      if (error || !data) {
         next(boom.unauthorized());
       }
 
-      req.login(data, { session: false }, async function(error) {
-        if(error) {
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
           next(error);
         }
 
@@ -31,7 +34,7 @@ app.post('/auth/sign-in', async function(req, res, next) {
 
         res.cookie('token', token, {
           httpOnly: !config.dev,
-          secure: !config.dev
+          secure: !config.dev,
         });
 
         res.status(200).json(user);
@@ -39,39 +42,37 @@ app.post('/auth/sign-in', async function(req, res, next) {
     } catch (error) {
       next(error);
     }
-  })(req, res, next)
+  })(req, res, next);
 });
 
-app.post('/auth/sign-up', async function(req, res, next) {
+app.post('/auth/sign-up', async function (req, res, next) {
   const { body: user } = req;
 
   try {
     await axios({
       url: `${config.apiUrl}/api/auth/sign-up`,
       method: 'post',
-      data: user
+      data: user,
     });
 
-    res.status(201).json({ message: 'user created'});
+    res.status(201).json({ message: 'user created' });
   } catch (error) {
     next(error);
   }
 });
 
-app.get('/movies', async function(req, res, next) {
+app.get('/movies', async function (req, res, next) {});
 
-});
-
-app.post('/user-movies', async function(req, res, next) {
+app.post('/user-movies', async function (req, res, next) {
   try {
     const { body: userMovie } = req;
     const { token } = req.cookies;
 
     const { data, status } = await axios({
       url: `${config.apiUrl}/api/user-movies`,
-      headers: { Authorization: `Bearer ${token}`},
+      headers: { Authorization: `Bearer ${token}` },
       method: 'post',
-      data: userMovie
+      data: userMovie,
     });
 
     if (status !== 201) {
@@ -84,14 +85,14 @@ app.post('/user-movies', async function(req, res, next) {
   }
 });
 
-app.delete('/user-movies/:userMovieId', async function(req, res, next) {
+app.delete('/user-movies/:userMovieId', async function (req, res, next) {
   try {
     const { userMovieId } = req.params;
     const { token } = req.cookies;
 
     const { data, status } = await axios({
       url: `${config.apiUrl}/api/user-movies/${userMovieId}`,
-      headers: { Authorization: `Bearer ${token}`},
+      headers: { Authorization: `Bearer ${token}` },
       method: 'delete',
     });
 
@@ -105,6 +106,32 @@ app.delete('/user-movies/:userMovieId', async function(req, res, next) {
   }
 });
 
-app.listen(config.port, function() {
+app.get(
+  '/auth/google-oauth',
+  passport.authenticate('google-oauth', {
+    scope: ['email', 'profile', 'openid'],
+  })
+);
+
+app.get(
+  '/auth/google-oauth/callback',
+  passport.authenticate('google-oauth', { session: false }),
+  function (req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie('token', token, {
+      httpOnly: !config.dev,
+      secure: !config.dev,
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
 });
